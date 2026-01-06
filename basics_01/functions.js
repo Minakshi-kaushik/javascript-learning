3b36655e381326b171139d8a87651de42ca65a43
Minakshi Kaushik <minakshikaushik22122005@gmail.com>
2026-01-06 21:23:17 +0530

    add scope and closures

--- basics_01/functions.js
+++ basics_01/functions.js
@@ -28,3 +28,46 @@ function handleObj(obj){
     //take parameters using the . n [ ] n string interpolation
 }
 // to pass the arrays same concept as the objs  ..both can be passed using the name or direct
+
+
+// Scope
+let a = 10;
+const b = 20
+var c = 3//donot work for the block scope
+//{} scope of the program
+//console.log(aor b )will not work but c will work
+
+//block scope & global scope
+
+//scope level and hoisting
+function one(){
+    const username = "abc"
+
+    function two(){
+        const website="Youtube"
+        console.log(username);
+    }
+    //console.log(website); gives error as parent function cannot access the vars of the child function
+
+     two();
+}
+
+one();
+
+//same in if else ...outer condition cannot access the inner condition variables...it is called closure
+
+
+
+//+++++++++++++++++++++++++++++++interesting
+
+function addone(num){
+    return num+1;
+}
+
+addone(7);
+
+const addTwo = function(num){//called expression...in JS variablws are very powerfull can hold json values or functions
+    return num+2;
+}
+
+addTwo(8);
