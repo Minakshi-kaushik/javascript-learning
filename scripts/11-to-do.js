ee46c7025b376a6b2c97d53c5c75b7885fbd4625
Minakshi Kaushik <minakshikaushik22122005@gmail.com>
2026-09-19 19:57:43 +0530

    add todoList second version

--- scripts/11-to-do.js
+++ scripts/11-to-do.js
@@ -1,13 +1,29 @@
 const todoList = ['make dinner','wash dishes'];
 
+renderToDoList();
+
+function renderToDoList(){
+let todoListHTML = '';
+
+for(let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p> ${todo}</p>`;
+    todoListHTML+=html;
+}
+console.log(todoListHTML);
+
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
 function renderTodoList(){
     let todoListHTML = '';
 
     for(let i = 0; i < todoList.length; i++){
         const todo = todoList[i];
 
-        const html = <p> ${todo}</p>
-        todoListHTM += html;
+        const html = `<p> ${todo}</p>`
+        todoListHTML += html;
     }
 
     console.log(todoListHTML);
@@ -18,12 +34,12 @@ function renderTodoList(){
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input')
 
-    
-
     const name = inputElement.value;
-    todoList.push([name]);
+    todoList.push(name);
     console.log(todoList);
 
     inputElement.value = '';
+
+    renderToDoList();
 
 }
